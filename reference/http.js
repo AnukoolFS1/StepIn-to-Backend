4533aa9532eea2978f146a6103d5c9c9c91a3a46
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-09-27 22:14:28 +0530

    revised http

--- reference/http.js
+++ reference/http.js
@@ -1,15 +1,25 @@
-// hypertext transfer protocol
+// // hypertext transfer protocol
 
-const http = require('http')
+// const http = require('http')
+
+// const serverListener = (req, res) => {
+//     res.write('backend initial stage');
+//     res.end()
+// }
+
+// const server = http.createServer(serverListener);
+
+// server.listen(6557, ()=>console.log('server initiated'));
+// // console.log('hi'); // to check if asynchronous capability of .listen Method
+
+
+const http = require('http');
 
 const serverListener = (req, res) => {
-    res.write('backend initial stage');
-    res.end()
+        res.writeHead(200,{"Content-Type": "text/html"});
+        res.end('<h1> Hello client </h1>')
 }
 
 const server = http.createServer(serverListener);
 
-server.listen(6557, ()=>console.log('server initiated'));
-// console.log('hi'); // to check if asynchronous capability of .listen Method
-
-
+server.listen(3000, ()=> console.log('server initited'))
