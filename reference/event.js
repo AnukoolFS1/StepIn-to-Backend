037f222d8f945698a949e781ca92ca47d73c5c9b
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-30 22:51:28 +0530

    added a second class

--- reference/event.js
+++ reference/event.js
@@ -1,8 +1,10 @@
 const EventEmitter = require('node:events');
 const PizzaShop = require('./extendEventEmitter')
 const emitter = new EventEmitter();
+const DrinkMachine = require('./events2')
 
 const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
 
 // emitter.on("test", (a,b) => {
 //     console.log(`${a} has initiated at this point`);
@@ -19,6 +21,7 @@ const pizzaShop = new PizzaShop();
 
 pizzaShop.on("order", (size, toppings) => {
     console.log(`order recieved of ${size} pizza with ${toppings} toppings`)
+    drinkMachine.serveDrink(size)
 })
 
 pizzaShop.order("large", "mushrooms")
