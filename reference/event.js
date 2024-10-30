a95ba9f0a56993e155449bf3b7694dcd1b23c18d
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-30 22:45:46 +0530

    pizza extends eventEmitter

--- reference/event.js
+++ reference/event.js
@@ -1,16 +1,25 @@
 const EventEmitter = require('node:events');
+const PizzaShop = require('./extendEventEmitter')
 const emitter = new EventEmitter();
 
-emitter.on("test", (a,b) => {
-    console.log(`${a} has initiated at this point`);
+const pizzaShop = new PizzaShop();
+
+// emitter.on("test", (a,b) => {
+//     console.log(`${a} has initiated at this point`);
+// })
+
+// emitter.on("test", (a,b) => {
+//     if(b === "random"){
+//         console.log(`Yes we can use the second :on over the same emit`)
+//     }
+// })
+
+// emitter.emit("test", "server", "random")
+// emitter.emit('test', "server2")
+
+pizzaShop.on("order", (size, toppings) => {
+    console.log(`order recieved of ${size} pizza with ${toppings} toppings`)
 })
 
-
-emitter.on("test", (a,b) => {
-    if(b === "random"){
-        console.log(`Yes we can use the second :on over the same emit`)
-    }
-})
-
-emitter.emit("test", "server", "random")
-emitter.emit('test', "server2")
+pizzaShop.order("large", "mushrooms")
+pizzaShop.displayOrderNumber()
--- reference/extendEventEmitter.js
+++ reference/extendEventEmitter.js
@@ -1,10 +1,14 @@
-class PizzaShop {
+const EventEmitter = require('node:events');
+
+class PizzaShop extends EventEmitter {
     constructor() {
+        super()
         this.orderNumber = 0
     }
 
-    order(){
+    order(size, toppings){
         this.orderNumber++
+        this.emit("order", size, toppings)
     }
 
     displayOrderNumber(){
