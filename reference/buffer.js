50883dbca23002bc66ae65621831cf1815adf791
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-06 16:34:08 +0530

    Buffer.alloc()

--- reference/buffer.js
+++ reference/buffer.js
@@ -1,9 +1,15 @@
-const buffer = new Buffer.from("Anukool");
+// const buffer = new Buffer.from("Anukool");
 
-console.log(buffer) // return the buffer in Hexadecimal
+// console.log(buffer) // return the buffer in Hexadecimal
 
-buffer.write('Applepool') 
+// buffer.write('Applepool') 
 
-console.log(buffer.toJSON()) // return in object of type:"Buffer", data: []
+// console.log(buffer.toJSON()) // return in object of type:"Buffer", data: []
 
-console.log(buffer.toString())
+// console.log(buffer.toString())
+
+
+const buffer = new Buffer.alloc(10)
+buffer.write("Anukool")
+
+console.log(buffer)
