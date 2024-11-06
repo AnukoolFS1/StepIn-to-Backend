4ec836bb01ab0a8f1b46c13d005b61c196afc041
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-06 16:56:07 +0530

    stream write

--- reference/stream.js
+++ reference/stream.js
@@ -1,10 +1,13 @@
 const fs = require('node:fs');
 
-const readableStream = fs.createReadStream("./reference/file1.txt", {
+const readableStream = fs.createReadStream("./file1.txt", {
     encoding:"utf-8",
     highWaterMark:3
 })
 
+const writeAbleStream = fs.createWriteStream("./file2.txt")
+
 readableStream.on("data", (chunk) => {
     console.log(chunk)
+    writeAbleStream.write(chunk)
 })
