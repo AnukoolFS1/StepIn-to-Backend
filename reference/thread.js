07675600d0b944dca2d86f1847fa7cce99c89e02
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-18 17:37:35 +0530

    crypto * 4

--- reference/thread.js
+++ reference/thread.js
@@ -2,7 +2,13 @@ const crypto = require('node:crypto');
 
 const start = Date.now();
 
-const data = crypto.pbkdf2Sync("password", "salt", 10000000, 512, "sha512");
+// const data = crypto.pbkdf2Sync("password", "salt", 10000000, 512, "sha512");
 
-console.log(Date.now() - start,"ms")
+for (let i = 0; i <= 3; i++) {
+    crypto.pbkdf2('password', 'salt', 100000, 512, "sha512", () => {
+        console.log(`hash ${i + 1} ${Date.now() - start}`)
+    })
+}
+
+console.log(Date.now() - start, "ms")
 
