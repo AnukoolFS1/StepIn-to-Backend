fd16b6a83d82ae6e72d8037338bde847b8a9d52d
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-18 17:49:44 +0530

    thread size increaseuped

--- reference/thread.js
+++ reference/thread.js
@@ -1,14 +1,14 @@
 const crypto = require('node:crypto');
 
+process.env.UV_THREADPOOL_SIZE = 10;
 const start = Date.now();
 
 // const data = crypto.pbkdf2Sync("password", "salt", 10000000, 512, "sha512");
 
-for (let i = 0; i <= 3; i++) {
+for (let i = 0; i < 10; i++) {
     crypto.pbkdf2('password', 'salt', 100000, 512, "sha512", () => {
         console.log(`hash ${i + 1} ${Date.now() - start}`)
     })
 }
 
 console.log(Date.now() - start, "ms")
-
